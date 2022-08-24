cde4cd00e95fb71f5758cc52c46a5e39c8806119
Timóteo Stifft <timoteostifft@gmail.com>
2022-08-24 09:48:01 -0300

    feat: create app default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  background: '#FAFAFA',
+  card: '#F3F2F2',
+  input: '#EDEDED',
+  button: '#E6E5E5',
+  hover: '#D7D5D5',
+  label: '#8D8686',
+  text: '#574F4D',
+  subtitle: '#403937',
+  title: '#272221',
+
+  'purple-200': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-700': '#4B2995',
+
+  'yellow-200': '#F1E9C9',
+  'yellow-500': '#DBAC2C',
+  'yellow-700': '#C47F17'
+}
