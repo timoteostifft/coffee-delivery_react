c5a5dac92c42b3ad2903ee4517eeb5d770cd0cb0
Timóteo Stifft <timoteostifft@gmail.com>
2022-08-24 10:27:35 -0300

    feat: create pages router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import Router from './Router'
 import { defaultTheme } from './styles/themes/default'
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        Hello World!
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
