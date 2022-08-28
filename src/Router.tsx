00b795ea725e10090307a8e6a9320edfe8733e7c
Timóteo Stifft <timoteostifft@gmail.com>
2022-08-28 17:37:18 -0300

    feat: create order page route

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,12 +4,14 @@ import { Routes, Route } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout';
 
 import Home from './pages/Home'
+import Order from './pages/Order';
 
 const Router: React.FC = () => {
   return (
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
         <Route path='/' element={<Home />} />
+        <Route path='/order' element={<Order />} />
       </Route>
     </Routes>
   );
