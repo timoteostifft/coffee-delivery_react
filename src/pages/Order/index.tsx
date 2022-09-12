c90fcd45d0089201c4ac00693e9dc7922ea3d200
Timóteo Stifft <timoteostifft@gmail.com>
2022-09-12 11:56:08 -0300

    feat: create order form provider

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,30 +1,69 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import { OrderContainer } from './styles';
-import { useForm } from 'react-hook-form';
+import { useForm, FormProvider } from 'react-hook-form';
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import Address from './components/Address';
 import Description from './components/Description';
+import { CartContext } from '../../contexts/CartContext';
+import { useNavigate } from 'react-router-dom';
+
+const newOrderFormValidationSchema = zod.object({
+  cep: zod.string().min(1, { message: 'Preencha este campo!' }).length(8, { message: 'Informe um CEP válido.' }),
+  street: zod.string().min(1, { message: 'Preencha este campo!' }),
+  number: zod.string().min(1, { message: 'Preencha este campo!' }),
+  complement: zod.string().min(1, { message: 'Preencha este campo!' }),
+  district: zod.string().min(1, { message: 'Preencha este campo!' }),
+  city: zod.string().min(1, { message: 'Preencha este campo!' }),
+  uf: zod.string().min(1, { message: 'Preencha este campo!' }).length(2, { message: 'Informe um UF válido.' }),
+  payment: zod.string().min(1, { message: 'Preencha este campo!' }),
+})
+
+export type newOrderFormData = zod.infer<typeof newOrderFormValidationSchema>
 
 const Order: React.FC = () => {
-  const { register, handleSubmit, formState: { errors }, watch } = useForm()
+  const { createNewOrder, finalizedOrders, cartItems } = useContext(CartContext)
 
-  function handleCreateNewOrder(data: any) {
-    console.log(data)
+  const newOrderForm = useForm<newOrderFormData>({
+    resolver: zodResolver(newOrderFormValidationSchema),
+    defaultValues: {
+      cep: '',
+      street: '',
+      number: '0',
+      complement: '',
+      district: '',
+      city: '',
+      uf: '',
+      payment: ''
+    }
+  })
+
+  useEffect(() => {
+    console.log(finalizedOrders)
+  }, [])
+
+  const { handleSubmit, reset } = newOrderForm
+
+  const navigate = useNavigate()
+
+  const navigateToSuccess = () => {
+    navigate('/success')
   }
 
-  const CEP = watch('CEP')
-  const isSubmitDisabled = false
+  function handleCreateNewOrder(data: newOrderFormData) {
+    createNewOrder(data)
+    reset()
+    navigateToSuccess()
+  }
 
   return (
     <OrderContainer>
       <form onSubmit={handleSubmit(handleCreateNewOrder)} action="">
-        <Address />
-        <Description />
-        {/* {errors.CEP && "CEP inválido."} */}
-
-        {/* <button type='submit' disabled={isSubmitDisabled}>
-          OK
-        </button> */}
+        <FormProvider {...newOrderForm}>
+          <Address />
+          <Description />
+        </FormProvider>
       </form>
     </OrderContainer>
   );
