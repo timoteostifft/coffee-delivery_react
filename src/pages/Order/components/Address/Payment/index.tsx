f114367448c2db40365246bd980ac61c658b4afb
Timóteo Stifft <timoteostifft@gmail.com>
2022-09-12 11:56:45 -0300

    feat: register order form radio buttons

--- src/pages/Order/components/Address/Payment/index.tsx
+++ src/pages/Order/components/Address/Payment/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import { Header, PaymentContainer } from './styles';
+import { Header, PaymentContainer, RadioButton, RadioButtons } from './styles';
 import money from '../../../../../assets/money.svg';
 import credit_card from '../../../../../assets/credit_card.svg';
 import debit_card from '../../../../../assets/debit_card.svg';
 import cash from '../../../../../assets/cash.svg';
+import { useFormContext } from 'react-hook-form';
 
 
 const Payment: React.FC = () => {
+  const { register, formState: { errors }, watch } = useFormContext()
+
+  let selectedRadioButton = watch('payment')
+
   return (
     <PaymentContainer>
       <Header>
@@ -18,20 +23,31 @@ const Payment: React.FC = () => {
         </div>
       </Header>
 
-      <div>
-        <button>
+      <RadioButtons>
+        <RadioButton htmlFor="credit_card"
+          background={selectedRadioButton === 'credit_card' ? 'hover' : 'default'}
+          border={selectedRadioButton === 'credit_card' ? 'hover' : 'default'}>
           <img src={credit_card} alt="" />
           <span>CARTÃO DE CRÉDITO</span>
-        </button>
-        <button>
+          <input {...register("payment")} type="radio" value="credit_card" id='credit_card' />
+        </RadioButton>
+
+        <RadioButton htmlFor="debit_card"
+          background={selectedRadioButton === 'debit_card' ? 'hover' : 'default'}
+          border={selectedRadioButton === 'debit_card' ? 'hover' : 'default'}>
           <img src={debit_card} alt="" />
           <span>CARTÃO DE DÉBITO</span>
-        </button>
-        <button>
+          <input {...register("payment")} type="radio" value="debit_card" id='debit_card' />
+        </RadioButton>
+
+        <RadioButton htmlFor="cash"
+          background={selectedRadioButton === 'cash' ? 'hover' : 'default'}
+          border={selectedRadioButton === 'cash' ? 'hover' : 'default'}>
           <img src={cash} alt="" />
           <span>DINHEIRO</span>
-        </button>
-      </div>
+          <input {...register("payment")} type="radio" value="cash" id='cash' />
+        </RadioButton>
+      </RadioButtons>
     </PaymentContainer>
   );
 }
